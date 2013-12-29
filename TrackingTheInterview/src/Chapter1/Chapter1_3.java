9db54b8cdd6b3f05543a1891d6fc6ea8633d08ff
cyu424 <Cyu424@usc.edu>
2013-12-29 10:36:48 -0800

    Update Chapter1_3.java

--- TrackingTheInterview/src/Chapter1/Chapter1_3.java
+++ TrackingTheInterview/src/Chapter1/Chapter1_3.java
@@ -34,19 +34,20 @@ public class Chapter1_3 {
 		
 		boolean[] hit = new boolean[256];
 		int len=a.length;
-		int c=0;
+		int c=0;                             //locate the index to add next qualified element
 		int i=0;
 		for(i=0;i<len;i++)
 		{
-			if(!hit[a[i]])
+			if(!hit[a[i]])            //by initial, hit[i]=false
 			{
 				a[c]=a[i];
 				c++;
-				hit[a[i]]=true;
+				hit[a[i]]=true;         //has already existed, next iteration if 
+                                                             //the same, don't give value.
 			}
 		}
 		for(int j=c;j<i;j++)
-			a[j]=0;          //¿Õ×Ö·û
+			a[j]=0;          //all space behind fill null(0)
 
 		
 		return a;
@@ -56,16 +57,17 @@ public class Chapter1_3 {
 	
 	public static char[] isE_two(char[] a){
 		int len = a.length;
-		int pos=1;             //new array to be inserted
+		int pos=1;             //position which new element to be inserted
 		for(int i=1;i<len;++i)
 		{
 			int j;
 			for(j=0;j<pos;++j)  //compare to pos,if same,break,compare next i
 			{
 				if(a[j]==a[i])					
-					break;
+					break;  // end current loop,next element to examine
 			}	                //compare all elements in new array before i
-				if(j==pos)      //compare done, if j = pos, shows that none is the same, insert
+				if(j==pos)      //compare done, if j = pos, shows that 
+                                                      //none is the same, insert
 				{	
 					a[pos]=a[i];
 					++pos;
